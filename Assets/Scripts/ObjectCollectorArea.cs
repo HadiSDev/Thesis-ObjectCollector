69b838a0cb6b72fa51a39f00291a0aaf8f021cc5
hadisdev <hads@itu.dk>
2022-04-14 16:48:54 +0200

    update

--- Assets/Scripts/ObjectCollectorArea.cs
+++ Assets/Scripts/ObjectCollectorArea.cs
@@ -1,12 +1,7 @@
-
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using MBaske.Sensors.Grid;
 using Unity.MLAgents;
 using Unity.MLAgentsExamples;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
